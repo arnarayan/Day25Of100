f6762fb95103ebf9b912b4462805258a193c1581
Anand.Narayan <arnarayan@gmail.com>
2022-12-12 06:44:03 -0700

    Initial Commit

---
+++ Day25Of100/Day25Of100App.swift
@@ -0,0 +1,17 @@
+//
+//  Day25Of100App.swift
+//  Day25Of100
+//
+//  Created by Anand Narayan on 2022-12-12.
+//
+
+import SwiftUI
+
+@main
+struct Day25Of100App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
